f7d0d14115f7b8b68931e362047c1823fc49e493
micsza <michal.szafraniuk@gmail.com>
2025-04-09 20:46:45 +0200

    initial

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,12 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
-
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://localhost:8080/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -15,8 +16,14 @@ async fn health_check_works() {
 
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to start zero2prod");
+fn spawn_app() -> String {
+    let address = "127.0.0.1:0";
+    let listener =
+        TcpListener::bind(address).expect(&format!("Failed to bind to {}", address));
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to start zero2prod");
 
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
